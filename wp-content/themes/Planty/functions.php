e291f1ab15d343ca0d8e1c3c4b7530a872e62647
Clément <clement.dhuskye@gmail.com>
2024-02-11 23:00:03 +0100

    2eme page fini, création de la 3eme sans l'input chiffre

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -13,10 +13,16 @@ function theme_enqueue_styles()
      // Chargement du /css/shortcodes/banniere-titre.css pour notre shortcode banniere titre
      wp_enqueue_style('banniere-titre-shortcode', get_stylesheet_directory_uri() . '/css/shortcodes/banniere-titre.css', 
      array(), filemtime(get_stylesheet_directory() . '/css/shortcodes/banniere-titre.css'));
+
+
+
+     // Shortcodes gouts commander 
+     wp_enqueue_style('gouts-commander-shortcode', get_stylesheet_directory_uri() . '/css/shortcodes/image-commander.css', 
+     array(), filemtime(get_stylesheet_directory() . '/css/shortcodes/image-commander.css'));
 }
 
 
-/* SHORTCODES */
+/* SHORTCODES banniere-titre */
 
 function banniere_titre_func($atts)
 {
@@ -48,4 +54,42 @@ function banniere_titre_func($atts)
 // Je dis à wordpress que j'ajoute un shortcode 'banniere-titre'
 add_shortcode('banniere-titre', 'banniere_titre_func');
 // Je génère le html retourné par mon shortcode
+
+
+
+
+/* Shortcodes commander gouts */ 
+
+function gouts_commander($atts)
+{
+    //Je récupère les attributs mis sur le shortcode
+    $atts = shortcode_atts(array(
+        'src' => '',
+        'titre' => 'Titre'
+    ), $atts, 'gouts-commander');
+    
+    //Je commence à récupéré le flux d'information
+    ob_start();
+
+    if ($atts['src'] != "") {
+        ?>
+
+<div class="gouts-commander" style="background-image: url(<?= $atts['src'] ?>)">
+    <h2 class="titre"><?= $atts['titre'] ?></h2>
+</div>
+
+
+<?php
+    }
+    
+    //J'arrête de récupérer le flux d'information et le stock dans la fonction $output
+    $output = ob_get_contents();
+    ob_end_clean();
+    
+    return $output;
+}
+// Je dis à wordpress que j'ajoute un shortcode 'banniere-titre'
+add_shortcode('gouts-commander', 'gouts_commander');
+// Je génère le html retourné par mon shortcode
+
 ?>
